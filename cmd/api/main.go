95da6dde10936fdbf51216b30b71c0adc8b33fac
HJH988 <hoongjiehua999@gmail.com>
2026-02-20 01:23:31 +0800

    setup of postgres connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"log"
+	"todo_api/internal/config"
+	"todo_api/internal/database"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
+	var cfg *config.Config
+	var err error
+	cfg, err = config.Load()
+
+	if err != nil {
+		log.Fatal("Failed to load configuration: ", err)
+	}
+
+	var pool *pgxpool.Pool
+	pool, err = database.Connect(cfg.DatabaseURL)
+
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+
+	defer pool.Close()
+
 	var router *gin.Engine = gin.Default()
 	router.SetTrustedProxies(nil)
 	router.GET("/", func(c *gin.Context) {
@@ -13,9 +35,10 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": "Todo API is Running well!",
 			"status":  "success",
+			"databse": "connected",
 		})
 	})
 
-	router.Run(":3000")
+	router.Run(":" + cfg.Port)
 
 }
